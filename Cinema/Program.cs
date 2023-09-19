a19f002a515d74ddb3e1ef99d88388158a3ef6de
boriskisiov <boriskisiov@gmail.com>
2023-09-19 11:39:26 +0300

    Add project files.

---
+++ Cinema/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.Design;
+
+namespace Cinema
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           string screening = Console.ReadLine();
+            int rows = int.Parse(Console.ReadLine());   
+            int columns = int.Parse(Console.ReadLine());
+
+            double totalPrice = rows * columns;
+
+            if (screening == "Premiere")
+            {
+                totalPrice = totalPrice * 12;
+            } else if (screening == "Normal")
+            {
+                totalPrice = totalPrice * 7.5;
+            }else if (screening == "Discount")
+            {
+                totalPrice *= 5;
+            }
+            Console.WriteLine($"{totalPrice:f2} Leva");
+        }
+    }
+}
